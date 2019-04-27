2c1c1ceacf77869e88a2f61355185ee265705f2c
xbanegas <xbanegas@gmail.com>
2019-04-27 12:53:29 -0400

    move init to provider

--- src/MyProvider.js
+++ src/MyProvider.js
@@ -6,10 +6,36 @@ import { withRouter } from "react-router-dom";
 
 export const MyContext = React.createContext();
 class MyProvider extends Component {
-  state = {
-    user: undefined,
-    pageTitle: "Share a Meal",
-  };
+  constructor(props){
+    super(props);
+    this.state = {
+      user: undefined,
+      pageTitle: "Share a Meal",
+      initialized: false
+    };
+  }
+
+  initFirebase () {
+    const firebaseConfig = {
+      apiKey: `${process.env.REACT_APP_FIREBASE_KEY}`,
+      authDomain: "661573101353.firebaseapp.com",
+      databaseURL: "https://emerge-19.firebaseio.com",
+      // projectId: "project-id",
+      // storageBucket: "project-id.appspot.com",
+      // messagingSenderId: "sender-id",
+    };
+
+    window.firebase.initializeApp(firebaseConfig);
+    return window.firebase.database
+  }
+
+  componentDidMount() {
+    if (!this.state.initialized){
+      this.initFirebase();
+      this.setState({initialized: true});
+    }
+  }
+
 
   render() {
     return (
