27057b7ac46396702e516a201dba5faac70ee6ed
sjhernandez <sjhernandez1968@gmail.com>
2019-04-27 10:41:55 -0400

    added react router

---
+++ src/MyProvider.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import axios from "axios";
+import _ from "lodash";
+
+import { withRouter } from "react-router-dom";
+
+export const MyContext = React.createContext();
+class MyProvider extends Component {
+  state = {
+    user: undefined,
+  };
+
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          ...this.state,
+        }}>
+        {" "}
+        {this.props.children}{" "}
+      </MyContext.Provider>
+    );
+  }
+}
+const MyConsumer = MyContext.Consumer;
+
+export { MyConsumer };
+export default withRouter(MyProvider);
